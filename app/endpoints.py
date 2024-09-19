9fd1431397f8d6bb17b1dc98c6e46edb70e7660d
Jung Rae Jang <jungrae.jang@gmail.com>
2024-09-19 04:55:21 -0400

    added more print

--- app/endpoints.py
+++ app/endpoints.py
@@ -67,7 +67,7 @@ async def generate_response_endpoint(request: Request, req_body: QueryRequest):
     try:
         # Extract session_id from cookies
         print("Request cookies:", request.cookies)
-        print("Request", request)
+        print("Request base URL", request.base_url)
         print("Request headers:", request.headers)
         print("Request url:", request.url)
         session_id = request.cookies.get("session_id")
