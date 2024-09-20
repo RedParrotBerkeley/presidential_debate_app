6ce9970560b1c75114d438fa1e58d495e11f5d70
Mike <129897918+RedParrotBerkeley@users.noreply.github.com>
2024-09-20 09:51:29 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -25,10 +25,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://debatebot-client.vercel.app",  # FE
-        "https://starfish-app-nm2zl.ondigitalocean.app",  # BE
-    ],
+    allow_origins=["https://debatebot-client.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
