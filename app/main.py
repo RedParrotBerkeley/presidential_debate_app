b420d92b79643738c9ca44638ccf1850954cf22b
Mike <129897918+RedParrotBerkeley@users.noreply.github.com>
2024-09-21 13:15:04 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # Import CORS middleware
 from app.endpoints import router  # Ensure this import points to your endpoints file
 from app.utils import (
     insert_into_database,
@@ -13,13 +14,25 @@ import os
 import openai
 
 # Load environment variables from the .env file located in the /app directory
-load_dotenv(dotenv_path=".env")
+#load_dotenv(dotenv_path="/app/.env")
 
 # Access the API key from environment variables
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Initialize FastAPI app
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://dbapi.hrfinnovation.org/",  # Main Branch FE
+        "https://dbapi-stag.hrfinnovation.org/"  # dev branch FE
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include the router from endpoints
 app.include_router(router)
